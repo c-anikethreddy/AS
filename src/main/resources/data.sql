eeebe9379783d775cf3e3f23d9717d72bca99757
aniketh reddy <anikethreddy3@gmail.com>
2022-03-31 09:18:36 +0530

    edited data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,3 @@
 /*insert into PENSIONER values(1,'root@gmail.com','heamshu','root');*/
-select * from PENSIONER;
+DELETE FROM PENSIONER;
+insert into PENSIONER values(1,'root@gmail.com','heamshu','root');
